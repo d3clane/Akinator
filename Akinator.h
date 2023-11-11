dbbb4655c82bb454fe0a01b449c7b9fccd7f2465
Arman <ovakimian.al@phystech.edu>
2023-11-12 00:12:38 +0300

    Akinator with guessing and printing mode

--- Akinator.h
+++ Akinator.h
@@ -1,4 +1,18 @@
 #ifndef AKINATOR_H
 #define AKINATOR_H
 
+enum class AkinatorErrors
+{
+    NO_ERR,
+
+    NO_DATA_BASE,
+};
+
+AkinatorErrors AkinatorLoad();
+void AkinatorGuessMode();
+
+AkinatorErrors AkinatorWriteData();
+
+void AkinatorShowTree();
+
 #endif // AKINATOR_H
