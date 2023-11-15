b97f87e858e7606f1ab95317f3cff5cfae6bc594
Arman <ovakimian.al@phystech.edu>
2023-11-15 21:41:53 +0300

    Moved string funcs

--- Common/InputOutputFuncs.h
+++ Common/InputOutputFuncs.h
@@ -4,11 +4,6 @@
 #include "stdio.h"
 
 int GetFirstNotSpaceChar(FILE* inStream = stdin);
-
-//TODO: перенос в другой файл
-const char* SkipSymbolsUntilStopChar (const char* string, const char stopChar);
-const char* SkipSymbolsWhileStatement(const char* string, int (*statementFunc)(int));
-
 void MyFgets(char* string, const size_t maxStringLen, FILE* inStream = stdin);
 
 #endif
